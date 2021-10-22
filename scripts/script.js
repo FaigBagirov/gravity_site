350f0a23b6ee385c3b7e84dcaa11dabf4b363a2c
FaigBagirov <bagirov3105@gmail.com>
2021-10-22 09:48:13 +0400

    glidejs apllied and working. Nav is not styled yet

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,8 @@
-// import splide from '@splidejs/splide';
-
-// window.addEventListener('load', function(){
-//   new splide( '.splide' ).mount();
-//   })
-
 document.addEventListener( 'DOMContentLoaded', function() {
-  var splide = new Splide( '.splide', {
-    type: 'loop',
-    perPage: 1.3,
-    gap: '30px',
+  new Glide('.glide', {
+    type: 'carousel',
+    perView: 1.3,
+    gap: '30',
+  }).mount()
 
-  } );
-  splide.mount();
 } );
